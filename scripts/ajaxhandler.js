781fe7ed5ec22cf58dbc16478ab3ec2963945048
T-Allen <tariku.allen@gmail.com>
2015-12-01 18:13:45 -0800

    added code to display videos, fixed index errors

--- scripts/ajaxhandler.js
+++ scripts/ajaxhandler.js
@@ -2,22 +2,36 @@ $("form#searchbox").submit(function( event ) {
 			event.preventDefault();
 			url = "/api/results.json?" + $(this).serialize();
 			$.getJSON(url).done(function(data) {
-				$('#results').empty();
-				$('#message').empty();
+				$('#tracks').empty();
+				$('#videos').empty();
+				$('message').empty();
 				if ("message" in data) {
-					$("message").append(data.message);
+					$("#message").append(data.message);
 				};
 				if ("query" in data){
-					var caption = "<strong>Results for: " + data.query + "</strong>";
-					$("#results").append(caption)
+					var caption = "<h2>" + data.query + "</h2>";
+					$("#track-name").append(caption)
 					
-					for (var i = 1; i <= data.tracks.length; i++) {
+					var tracks = "<h3>Tracks</h3>"
+					$("#tracks").append(tracks)
+					for (var i = 1; i < data.tracks.length; i++) {
 						var trackInfo = $("<div></div>");
 						var title = "<p>" + data.tracks[i].track_title + "</p>";
 						var user = "<p>" + data.tracks[i].user + "</p>";
 						trackInfo.append(title);
 						trackInfo.append(user);
-						$("#message").append(trackInfo);
+						$("#tracks").append(trackInfo);
+					};
+
+					var videos = "<h3>Videos</h3>"
+					$("#videos").append(videos)
+					for (var i = 1; i < data.videos.length; i++) {
+						var trackInfo = $("<div></div>");
+						var title = "<p>" + data.videos[i].track_title + "</p>";
+						var user = "<p>" + data.videos[i].user + "</p>";
+						trackInfo.append(title);
+						trackInfo.append(user);
+						$("#videos").append(trackInfo);
 					};
 				};	
 			});
