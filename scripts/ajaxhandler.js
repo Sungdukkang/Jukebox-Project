42cb77d223cf931b2f09a0f554eebfe22151a64d
sungduk kang <sungduk.kang.94@gmail.com>
2015-12-05 18:39:02 -0800

    Did some more formatting and design. Tried to make an addToQueue method in ajaxhandler.js but it doesnt work

--- scripts/ajaxhandler.js
+++ scripts/ajaxhandler.js
@@ -11,26 +11,28 @@ $("form#searchbox").submit(function( event ) {
 					$("#message").append(data.message);
 				};
 				if ("query" in data){
-					var caption = "<h2> Results for '" + data.query + "': </h2>";
+					var caption = "<h3> Results for '" + data.query + "': </h3>";
 					$("#track-name").append(caption)
 					
 					if (data.tracks.length > 1) {
 						$("#tracks-container").show();
 						for (var i = 1; i < data.tracks.length; i++) {
+							var currTrack = data.tracks[i];
 							var container = document.createElement('div');
+						    $(container).attr('id', currTrack.stream_url);
 						    $(container).addClass('search-results clearfix');
 							
 							var artwork = document.createElement('img');
 							$(artwork).addClass('artwork');
-							if(data.tracks[i].artwork != 'None') {
-								$(artwork).attr('src', data.tracks[i].artwork); 
+							if(currTrack.artwork != 'None') {
+								$(artwork).attr('src', currTrack.artwork); 
 							} else {
 								$(artwork).attr('src', '/assets/default-art.png')
 							}
 
 							var trackInfo = document.createElement('div');
-							var title = "<p>" + data.tracks[i].track_title + "</p>";
-							var user = "<p>" + data.tracks[i].user + "</p>";
+							var title = "<p>" + currTrack.track_title + "</p>";
+							var user = "<p>" + currTrack.user + "</p>";
 							$(trackInfo).append(title);
 							$(trackInfo).append(user);
 
@@ -39,6 +41,15 @@ $("form#searchbox").submit(function( event ) {
 
 							$("#tracks").append(container);
 						};
+
+					/* TODO: Fix this 
+					    for(var j = 1; j < data.tracks.length; j++) {
+					    	var temp = data.tracks[j];
+					    	$(document.getElementById(temp.stream_url)).click(function() {
+						    	addToQueue(temp.track_title);
+						    });
+					    };
+					*/
 					};
 
 					if (data.videos.length > 1) {
@@ -66,3 +77,11 @@ $("form#searchbox").submit(function( event ) {
 				};	
 			});
 		});
+
+// TODO: Use this to add to the queue
+function addToQueue(trackObj) {
+	var item = document.createElement('li');
+	var title = "<p>" + trackObj+ "</p>";
+	$(item).append(title);
+	$('#queue').append(item);
+};
